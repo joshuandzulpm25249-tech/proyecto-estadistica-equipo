e670e31dccb55887f2654e2c05909ae9ee690fa3
darinel Alexander Canul Colli <darinel.canul._pm25257@cecyte.edu.mx>
2026-04-22 04:09:13 -0600

    se documenta el codigo de la funcion multiplicacion y division

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def multiplicacion (a,b):
     rest = a*b
     return rest 
+"""
+devuelve el resultado de multiplicacion.
+Args:
+    a(float): Primer valor a multiplicar.
+    b(float): Segundo valor a multiplicar.
 
+Return:
+    rest(float):resultado de la multiplicacion.
+"""
 def divicion (a,b):
     rest = a/b
     return rest 
+"""
+devuelve el resultado de divicion.
+Args:
+    a(float): Primer valor a dividir.
+    b(float): Segundo valor a dividir.
 
+Return:
+    rest(float):Resultado de la divicion:
+"""
 def main():
     print("---Analizador de datos v1.0---")
     datos = [10,20,30,40,50,60] #datos de prueba
